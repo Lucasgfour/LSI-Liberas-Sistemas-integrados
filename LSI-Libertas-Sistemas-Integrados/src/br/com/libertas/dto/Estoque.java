d631224991ba8eb94d4f4a90b43f906dbc226932
Marques Alexandre <64232193+Marques-Alexandre@users.noreply.github.com>
2020-10-12 13:53:17 -0300

    Up Classe Estoque

---
+++ LSI-Libertas-Sistemas-Integrados/src/br/com/libertas/dto/Estoque.java
@@ -0,0 +1,28 @@
+package br.com.libertas.dto;
+
+public class Estoque {
+	private int id;
+	private int produto;
+	private int quantidade;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getProduto() {
+		return produto;
+	}
+	public void setProduto(int produto) {
+		this.produto = produto;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
+}
